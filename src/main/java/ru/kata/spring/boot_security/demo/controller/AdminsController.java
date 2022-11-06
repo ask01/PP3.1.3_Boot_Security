d366a70eef90ef6c5e9a3f1927b1a6fc01a0439d
ask01 <alnetesov@ya.ru>
2022-11-06 13:44:56 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminsController.java
@@ -0,0 +1,58 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+@Controller
+@RequestMapping()
+public class AdminsController {
+    private final UserServiceImpl userService;
+
+    public AdminsController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/admin")
+    public String getAllUsers(Model model) {
+        model.addAttribute("user", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/admin/new")
+    public String newUser(Model model){
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping("/admin")
+    public String create(@ModelAttribute("user") User user) {
+        userService.create(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "show";
+    }
+
+    @GetMapping("/admin/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("/admin/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.update(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/admin/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,84 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDaoImpl;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private UserDaoImpl userDao;
+
+
+    public UserServiceImpl(UserDaoImpl userDao) {
+        this.userDao = userDao;
+    }
+
+    public User getUserByLogin(String login) {
+        return userDao.getUserByLogin(login);
+    }
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+
+        User user = userDao.getUserByLogin(login);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", login));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRoleToAuthorities(user.getRoles()));
+    }
+
+
+
+    private Collection<? extends GrantedAuthority> mapRoleToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getRole())).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+            return userDao.getUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+            return userDao.getUserById(id);
+    }
+
+    @Override
+    public User delete(long id) {
+        User user = null;
+        try {
+            user = userDao.deleteUser(id);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    @Override
+    public void create(User user)  {
+        userDao.createUser(user);
+    }
+
+    @Override
+    public void update(int id, User user) {
+        userDao.updateUser(user);
+    }
+}
+
+
+
